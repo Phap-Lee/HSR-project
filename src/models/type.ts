b80922dbac731a5faf7ce8fd228d04ed7d4a8098
Phap-Lee <125343278+Phap-Lee@users.noreply.github.com>
2026-05-20 16:52:25 +0700

    Beginnings of broad Types

---
+++ src/models/type.ts
@@ -0,0 +1,23 @@
+export const Paths = {
+    Hunt: 'The Hunt',
+    Preservation: 'The Preservation',
+    Erudition: 'The Erudition',
+    Abundance: 'The Abundance',
+    Harmony: 'The Harmony',
+    Nihility: 'The Nihility',
+    Destruction: 'The Destruction',
+    Rememberance: 'The Rememberance',
+    Elation: 'The Elation'
+};
+
+export type Elements =
+| 'Fire' | 'Ice' | 'Imaginary' | 'Physical' | 'Quantum' | 'Lightning' | 'Wind';
+
+export type StatKey =
+| 'Hp' | 'Atk' | 'Def'
+| 'Spd' | 'Cr' | 'Cdmg'
+| 'Ehr' | 'Er' | 'Be'
+| 'Err'
+| 'Fire_DMG_Bonus' | 'Ice_DMG_Bonus' | 'Imaginary_DMG_Bonus' | 'Physical_DMG_Bonus' | 'Quantum_DMG_Bonus' | 'Lightning_DMG_Bonus' | 'Wind_DMG_Bonus'
+| 'Basic_DMG_Bonus' | 'Skill_DMG_Bonus' | 'Ultimate_DMG_Bonus' | 'Fua_DMG_Bonus'
+| 'All_DMG_Bonus'
